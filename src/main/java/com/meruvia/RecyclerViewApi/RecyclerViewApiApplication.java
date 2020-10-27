e52c8533b9b78a4979708c202b69e864bcc4a338
meruvia <gmeruviarojas@gmail.com>
2020-10-27 09:25:51 -0400

    CREACION DE PROYECTO NUEVO

---
+++ src/main/java/com/meruvia/RecyclerViewApi/RecyclerViewApiApplication.java
@@ -0,0 +1,13 @@
+package com.meruvia.RecyclerViewApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclerViewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclerViewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meruvia/RecyclerViewApi/RecyclerViewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meruvia.RecyclerViewApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyclerViewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
